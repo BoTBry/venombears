05943fbd3c9c7e449d26cea3d21fb6f6d2ae5864
Bryan <116608338+BoTBry@users.noreply.github.com>
2023-07-20 23:06:53 +0100

    Add files via upload

---
+++ about/js/popup.js
@@ -0,0 +1,61 @@
+function togglePopup() {
+    var popup = document.getElementById("popup-1");
+    popup.classList.toggle("active");
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup-1");
+    popup.classList.remove("active");
+}
+
+function togglePopup2() {
+    var popup = document.getElementById("popup-2");
+    popup.classList.toggle("active");
+}
+
+function closePopup2() {
+    var popup = document.getElementById("popup-2");
+    popup.classList.remove("active");
+}
+
+function togglePopup3() {
+    var popup = document.getElementById("popup-3");
+    popup.classList.toggle("active");
+}
+
+function closePopup3() {
+    var popup = document.getElementById("popup-3");
+    popup.classList.remove("active");
+}
+
+function togglePopup4() {
+    var popup = document.getElementById("popup-4");
+    popup.classList.toggle("active");
+}
+
+function closePopup4() {
+    var popup = document.getElementById("popup-4");
+    popup.classList.remove("active");
+}
+
+function togglePopup5() {
+    var popup = document.getElementById("popup-5");
+    popup.classList.toggle("active");
+}
+
+function closePopup5() {
+    var popup = document.getElementById("popup-5");
+    popup.classList.remove("active");
+}
+
+function togglePopup6() {
+    var popup = document.getElementById("popup-6");
+    popup.classList.toggle("active");
+}
+
+function closePopup6() {
+    var popup = document.getElementById("popup-6");
+    popup.classList.remove("active");
+}
+
+
